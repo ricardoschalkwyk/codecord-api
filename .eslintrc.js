7d2497de1cf5d3c7e14e910114d6bc25964707a8
Ricardo Schalkwyk <schalkwykr24@gmail.com>
2022-08-07 18:13:44 +0200

    COD-14: Added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: false
+    node: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
